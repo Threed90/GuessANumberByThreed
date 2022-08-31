56974053729d2b15e5d0337c103904c4dfa516d9
Threed90 <threed@abv.bg>
2022-08-31 11:04:14 +0300

    Adding the initial game implementation.

---
+++ GuessANumber.cs
@@ -0,0 +1,32 @@
+﻿Random rdm = new Random();
+int number = rdm.Next(1, 101);
+
+while (true)
+{
+    Console.Write("Guess a number (1-100): ");
+    string userInput = Console.ReadLine();
+
+    bool isValid = int.TryParse(userInput, out int userNumber);
+
+    if (isValid)
+    {
+        if (number == userNumber)
+        {
+            Console.WriteLine("You guessed it!");
+            break;
+        }
+        else if(number < userNumber)
+        {
+            Console.WriteLine("Too High");
+        }
+        else
+        {
+            Console.WriteLine("Too Low");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input.");
+    }
+    
+}
